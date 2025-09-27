3dc513cafbb9a8984eb34127b856e78b91cb9fe5
fepaiao <felipepaiao04@gmail.com>
2025-09-27 01:17:54 -0300

    criando o primeiro crud de alunos e configurando o projeto

--- organizador_backend/bootstrap/app.php
+++ organizador_backend/bootstrap/app.php
@@ -7,12 +7,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Exclui a rota /alunos da verificação CSRF
+        $middleware->validateCsrfTokens(except: [
+            'alunos/*', 
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
--- organizador_backend/routes/web.php
+++ organizador_backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\AlunoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome'); //pagina do laravel
 });
+
+//Route::get('/alunos', [AlunoController::class, 'index']);   //rotas comentadas para teste, antes o api.php nao funcionava
+//Route::post('/alunos', [AlunoController::class, 'store']);
+//Route::put('/alunos/{id}', [AlunoController::class, 'update']);
+//Route::delete('/alunos/{id}', [AlunoController::class, 'destroy']);
